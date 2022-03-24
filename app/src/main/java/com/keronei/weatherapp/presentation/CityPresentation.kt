8f5ac11e391645bd924cafdfcdd50356e3743502
keronei <lincolnkeronei95@gmail.com>
2022-03-24 11:13:21 +0300

    Add country to city listing

--- app/src/main/java/com/keronei/weatherapp/presentation/CityPresentation.kt
+++ app/src/main/java/com/keronei/weatherapp/presentation/CityPresentation.kt
@@ -3,6 +3,7 @@ package com.keronei.weatherapp.presentation
 data class CityPresentation(
     val id: Int,
     val name: String,
+    val country : String,
     val temperature: String,
     val iconName: String
 )
