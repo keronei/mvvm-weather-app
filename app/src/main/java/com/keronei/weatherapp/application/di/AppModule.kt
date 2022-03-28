460955978128fa70ba7b8c4d7344382e96492dde
keronei <lincolnkeronei95@gmail.com>
2022-03-28 11:21:13 +0300

    Clean up unused methods after migration

--- app/src/main/java/com/keronei/weatherapp/application/di/AppModule.kt
+++ app/src/main/java/com/keronei/weatherapp/application/di/AppModule.kt
@@ -1,16 +1,12 @@
 package com.keronei.weatherapp.application.di
 
 import android.content.Context
-import com.fasterxml.jackson.dataformat.csv.CsvMapper
-import com.fasterxml.jackson.dataformat.csv.CsvSchema
-import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.keronei.weatherapp.application.preference.DataStoreManager
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import java.io.FileReader
 import javax.inject.Singleton
 
 @Module
@@ -21,14 +17,4 @@ object AppModule {
     @Singleton
     fun providesDataStoreInstance(@ApplicationContext context: Context): DataStoreManager =
         DataStoreManager(context)
-
-    @Provides
-    @Singleton
-    fun providesCsvJacksonInstance(): CsvMapper {
-        return CsvMapper().apply {
-            registerModule(KotlinModule.Builder().build())
-        }
-
-
-    }
 }
