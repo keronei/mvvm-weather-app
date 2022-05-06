5503ceb4df72d688b22926e5ddfae613190d9511
Keronei <lincolnkeronei95@gmail.com>
2022-05-06 12:25:30 +0300

    Change Worker to CoroutineWorker
    This provides doWork in background which has better support for suspend ops.

--- app/src/main/java/com/keronei/weatherapp/core/worker/NotificationWorker.kt
+++ app/src/main/java/com/keronei/weatherapp/core/worker/NotificationWorker.kt
@@ -19,7 +19,7 @@ import android.content.Context
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.hilt.work.HiltWorker
-import androidx.work.Worker
+import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.keronei.weatherapp.R
 import com.keronei.weatherapp.application.Constants.NOTIFICATION_CHANNEL_ID
@@ -30,20 +30,18 @@ import com.keronei.weatherapp.utils.toCelsius
 import com.keronei.weatherapp.utils.trimDecimalThenToString
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
-import java.lang.Exception
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.withContext
 import java.util.*
 
 @HiltWorker
 class NotificationWorker @AssistedInject constructor(
     @Assisted context: Context,
     @Assisted params: WorkerParameters,
-    private val citiesRepository: CitiesRepository,
-    private val coroutineScope: CoroutineScope
-) : Worker(context, params) {
-    override fun doWork(): Result {
+    private val citiesRepository: CitiesRepository
+) : CoroutineWorker(context, params) {
+    override suspend fun doWork(): Result {
         /**
          * Query all favourite cities
          * Check if there's data for current hour
@@ -51,37 +49,41 @@ class NotificationWorker @AssistedInject constructor(
          */
 
         try {
-            coroutineScope.launch {
-                citiesRepository.queryFavouritedCities().collect { favourited ->
+            val favourites = citiesRepository.queryFavouritedCities().first()
 
-                    /**
-                     * Get all favourite AND with data
-                     */
-                    val favoriteCities =
-                        favourited.filter { cityWithForecast -> cityWithForecast.forecast != null }
+            /**
+             * Get all favourites AND with data
+             */
+            val favoriteCities =
+                favourites.filter { cityWithForecast -> cityWithForecast.forecast != null }
 
-                    if (favoriteCities.isEmpty()) return@collect
+            if (favoriteCities.isEmpty()) {
+                return Result.failure()
+            }
 
-                    val citiesForNotification =
-                        favoriteCities.filter { city ->
-                            shouldAddCityToUpdatesForDispatch(city)
-                        }
+            val citiesForNotification =
+                favoriteCities.filter { city ->
+                    shouldAddCityToUpdatesForDispatch(city)
+                }
+            if (citiesForNotification.isEmpty()) {
+                return Result.failure()
+            }
 
-                    citiesForNotification.forEach { city ->
-                        displayNotification(applicationContext, city)
-                    }
-
+            citiesForNotification.forEach { city ->
+                withContext(Dispatchers.Main) {
+                    displayNotification(applicationContext, city)
                 }
             }
+            /**
+             * The assumption is that the notification would be handled by the system.
+             */
+
+            return Result.success()
+
         } catch (exception: Exception) {
             exception.printStackTrace()
             return Result.failure()
         }
-
-        /**
-         * The assumption is that the notification would be handled by the system.
-         */
-        return Result.success()
     }
 
     private fun shouldAddCityToUpdatesForDispatch(cityWithForecast: CityWithForecast): Boolean {
@@ -94,16 +96,17 @@ class NotificationWorker @AssistedInject constructor(
 
         // Get the particular hour
         val hour = cityWithForecast.forecast!!.hourly.first { hourly ->
-                hourly.dt.fromUnixTimestamp() == getCurrentHour().timeInMillis
-            }
+            hourly.dt.fromUnixTimestamp() == getCurrentHour().timeInMillis
+        }
 
-        val header = context.getString(R.string.city_hourly_update_suffix,
+        val header = context.getString(
+            R.string.city_hourly_update_suffix,
             cityWithForecast.cityObjEntity.city_ascii
         )
 
         val temperatureAsStringMessage = context.getString(R.string.actual_update_text,
             hour.temp.toCelsius().trimDecimalThenToString(context),
-                    hour.weather.first().description
+            hour.weather.first().description.replaceFirstChar { letter -> letter.uppercase() }
         )
 
         // Parse the icon
